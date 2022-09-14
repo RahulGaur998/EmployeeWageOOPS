701da15472d273b15e9ae65b0aa33d303d8fc240
RahulGaur998 <gaur.rahul996@gmail.com>
2022-09-14 18:51:50 +0530

    EDIT: Added first uc1 i.e. Added employee is present or absent

--- src/main/java/com/employeewageoops/App.java
+++ src/main/java/com/employeewageoops/App.java
@@ -1,8 +1,26 @@
 package com.employeewageoops;
 
+class Emp {
+    long isAbsent;
+
+    Emp() {
+        isAbsent = Math.round(Math.random());
+    }
+
+    void isAbsentPresent() {
+        if (isAbsent == 0) {
+            System.out.println("Present");
+        } else {
+            System.out.println("Absent");
+        }
+    }
+
+}
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
-
+        Emp empObject = new Emp();
+        empObject.isAbsentPresent();
     }
 }
