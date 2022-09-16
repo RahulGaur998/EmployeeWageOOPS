25a57ec89e8cca9779da81484caa975dc286829b
RahulGaur998 <gaur.rahul996@gmail.com>
2022-09-16 17:55:02 +0530

    EDIT: Completed logic for Salary of full month

--- src/main/java/com/employeewageoops/App.java
+++ src/main/java/com/employeewageoops/App.java
@@ -5,6 +5,8 @@ class Employee {
     int wagePerHour = 20;
     int partTimeOrFullTime;
     int salary;
+    int maxWorkingHours = 100;
+    int maxWorkingDays = 20;
 
     Employee() {
         isAbsent = (int) Math.round(Math.random()); // if isAbsent = 1 employee is absent else present
@@ -21,26 +23,37 @@ class Employee {
         }
     }
 
-    void employeeDailyWage() { // To calculate daily wage of the employee for the
+    int employeeDailyWage() { // To calculate daily wage of the employee for the
         switch (isAbsent) { // switch on the basis of isAbsent i.e Employee is present or not
             case 1:
                 salary = 0; // Employee is absent
                 break;
             case 0: // Employee is present
-                salary = partTimeOrFullTime * wagePerHour;
+                if (partTimeOrFullTime == 1) {
+                    salary = 4 * wagePerHour;
+                } else {
+                    salary = 8 * wagePerHour;
+                }
+
                 break;
             default:
                 System.out.println("No output!");
         }
         System.out.println("Salary for the day is : " + salary);
+        return salary;
     }
 }
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation");
-        Employee empObject = new Employee();
-        empObject.isAbsentPresent();
-        empObject.employeeDailyWage();
+        Employee empObject[] = new Employee[20];
+        int totalSalary = 0;
+        for (int i = 1; i < 20; i++) {
+            empObject[i] = new Employee();
+            empObject[i].isAbsentPresent();
+            totalSalary = totalSalary + empObject[i].employeeDailyWage();
+        }
+        System.out.println("Salary total is :" + totalSalary);
     }
 }
